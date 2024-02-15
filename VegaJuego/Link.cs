e62b2074da0075aaf394d2f6d3aa9344658db3f9
Esteban Martínez-Abarca Gabarda <esteban.martinez-abarca.gabarda@ces-vegamedia.es>
2024-02-15 09:22:23 +0100

    Empezando con la clase Link

---
+++ VegaJuego/Link.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace VegaJuego
+{
+    internal class Link : Personaje
+    {
+        private int salud;
+        private int escudo;
+        private int arma;
+
+        public Link(Level1 vegaZelda, int x, int y, int s, int e, int a): base(vegaZelda, x, y)
+        {
+
+            salud = s; 
+            escudo=e; 
+            arma = a;
+        }
+
+        public int Arma
+        {
+            get { return arma;}
+            set { arma = value; }
+        }
+
+        public int Escudo
+        {
+            get { return escudo; }
+            set { escudo = value; }
+        }
+        public int Salud
+        {
+            get { return salud; }
+            set { salud = value; }
+        }
+
+        public void mover(KeyPressEventArgs e)
+        {
+            if(e.KeyChar== 'w' && (Caja.Location.Y > 0))
+            {
+                this.Direccion = "ARRIBA";
+                this.Caja.BackgroundImage = global::VegaJuego.Properties.Resources.Arriba;
+                this.Caja.Location = new Point(this.Caja.Location.X, this.Caja.Location.Y - 10);
+            }
+            if(e.KeyChar== 's' && (Caja.Location.Y < Lienzo.Size.Height -160))
+            {
+                this.Direccion = "ABAJO";
+
+            }
+        }
+    }
+}
---
+++ VegaJuego/Personaje.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VegaJuego
+{
+    internal class Personaje
+    {
+        private int coordX;
+        private int coordY;
+        private Level1 lienzo;
+        private PictureBox caja= new PictureBox();
+        private string direccion;
+
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+
+        public int CoordY
+        {
+            get { return coordY; }
+            set { coordY = value; }
+        }
+
+        public int CoordX
+        {
+            get { return coordX; }
+            set { coordX = value;  }
+        }
+
+        public Level1 Lienzo
+        {
+            get { return lienzo; }
+            set { lienzo = value; }
+        }
+
+        public PictureBox Caja
+        {
+            get { return caja; }
+            set { caja = value; }
+        }
+
+        public void mover()
+        {
+        }
+
+        public Personaje(Level1 vegaZelda, int x. int y)
+        {
+            coordX = x;
+            coordY = y;
+            lienzo = vegaZelda;
+            direccion = "Derecha";
+            caja.Location= new System.Drawing.Point(coordX, coordY);
+            caja.BackColor = System.Drawing.Color.Transparent;
+            caja.Size = new System.Drawing.Size(110, 110);
+            caja.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            caja.BackgroundImage = global::VegaJuego.Properties.Resources.derecha;
+
+            vegaZelda.Controls.Add(caja); caja.Visible = true;
+        }
+    }
+}
