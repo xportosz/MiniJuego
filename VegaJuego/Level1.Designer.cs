7fc2aed1cdb9dadc9a8c93c8d83daaeeb0024b57
Esteban Martínez-Abarca Gabarda <esteban.martinez-abarca.gabarda@ces-vegamedia.es>
2024-02-20 12:26:02 +0100

    Ultimos cambios con los corazones

--- VegaJuego/Level1.Designer.cs
+++ VegaJuego/Level1.Designer.cs
@@ -28,8 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Level1));
+            MovEnemigos = new System.Windows.Forms.Timer(components);
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // MovEnemigos
+            // 
+            MovEnemigos.Enabled = true;
+            MovEnemigos.Tick += timer1_Tick;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = Properties.Resources.salud;
+            pictureBox1.Location = new Point(639, 101);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 50);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(762, 121);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
             // 
             // Level1
             // 
@@ -37,6 +66,8 @@
             BackgroundImage = Properties.Resources.FondoFinal;
             BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(806, 467);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
             DoubleBuffered = true;
             Icon = (Icon)resources.GetObject("$this.Icon");
             MaximizeBox = false;
@@ -44,9 +75,15 @@
             StartPosition = FormStartPosition.CenterScreen;
             Text = "VegaZelda";
             KeyPress += Level1_KeyPress_1;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer MovEnemigos;
+        private PictureBox pictureBox1;
+        private Label label1;
     }
 }
--- VegaJuego/Level1.cs
+++ VegaJuego/Level1.cs
@@ -15,11 +15,21 @@ namespace VegaJuego
             Enemigo1 = new Broncas(this, 0, 300);
         }
 
-    
+
 
         private void Level1_KeyPress_1(object sender, KeyPressEventArgs e)
         {
             Player.mover(e);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Enemigo1.mover();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
